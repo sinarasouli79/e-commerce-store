0bfa755bf9638b6158cd125b1ec8f03668c77603
sinarasouli79 <sinarasouli79@gmail.com>
2022-05-26 20:26:40 +0430

    convert class-based views to generic views

--- src/store/views.py
+++ src/store/views.py
@@ -1,42 +1,19 @@
-from urllib import response
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.views import Response
-from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
+
 from .models import Product
 from .serializers import ProductSerializer
 
 # Create your views here.
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        # deserialized
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+class ProductList(ListCreateAPIView):
 
-class ProdcutDetail(APIView):
-    def get(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-    def put(self, request, id):        
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(instance=product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
 
-    def delete(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)    
+class ProdcutDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
