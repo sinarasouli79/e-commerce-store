c503decc5fff1e6875d9993f83079dec73bcfea9
sinarasouli79 <sinarasouli79@gmail.com>
2022-05-26 21:28:12 +0430

    store/models/Review model class created.
    make migrations and migrations were did.

--- src/store/models.py
+++ src/store/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,10 @@ class Product(models.Model):
     inventory = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     last_update = models.DateTimeField(auto_now=True)
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
