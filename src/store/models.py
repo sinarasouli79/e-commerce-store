60943c7d0ad546d36907a7ac56b2a2d3f3031992
sinarasouli79 <sinarasouli79@gmail.com>
2022-05-31 20:26:19 +0430

    Product and Customer added to the admin and and some changes in the store/models

--- src/store/models.py
+++ src/store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.contrib import admin
+
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -36,3 +38,10 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
     
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
