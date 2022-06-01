3ec1175983dada9ff0fd706a75cea69f4369af1c
sinarasouli79 <sinarasouli79@gmail.com>
2022-06-01 17:08:34 +0430

    collection added to the serializer

--- src/store/serializers.py
+++ src/store/serializers.py
@@ -8,7 +8,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'title', 'description',
-                  'inventory', 'unit_price', 'last_update']
+                  'inventory', 'unit_price', 'last_update', 'collection']
 
     unit_price = serializers.DecimalField(
         max_digits=6, decimal_places=2, source='price')
