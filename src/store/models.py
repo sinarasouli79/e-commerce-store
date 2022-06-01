703891c600037f0a72d26a3ca8abb70378892ac0
sinarasouli79 <sinarasouli79@gmail.com>
2022-06-01 13:35:17 +0430

    Collection and Order models created

--- src/store/models.py
+++ src/store/models.py
@@ -3,19 +3,28 @@ from django.conf import settings
 from django.contrib import admin
 
 # Create your models here.
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     inventory = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     last_update = models.DateTimeField(auto_now=True)
+    collectoin = models.ForeignKey('Collection', on_delete=models.PROTECT)
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
 
 
 class Reviews(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='reviews')
     name = models.CharField(max_length=255)
     description = models.TextField()
     date = models.DateField(auto_now_add=True)
+
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -33,11 +42,13 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
-    
+
     @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name
@@ -45,3 +56,24 @@ class Customer(models.Model):
     @admin.display(ordering='user__last_name')
     def last_name(self):
         return self.user.last_name
+
+
+class Order(models.Model):
+    '''
+    payment status
+    placed at
+    customer
+    date 
+    '''
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [(PAYMENT_STATUS_PENDING, 'PENDING'), (
+        PAYMENT_STATUS_COMPLETE, 'COMPLETE'), (PAYMENT_STATUS_FAILED, 'FAILED')]
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+
+    # never delete orders from our data base because they are represent our sales
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
